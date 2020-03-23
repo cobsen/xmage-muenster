0577a4979fd3f95db1c52165b850bc51c60bfe95
cobsen <jakob@perspective-daily.de>
2020-03-23 16:24:18 +0100

    initial server setup

---
+++ src/main/java/mage/server/game/GamesRoomManager.java
@@ -0,0 +1,57 @@
+package mage.server.game;
+
+import org.apache.log4j.Logger;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author BetaSteward_at_googlemail.com
+ */
+public enum GamesRoomManager {
+    instance;
+
+    private final ConcurrentHashMap<UUID, GamesRoom> rooms = new ConcurrentHashMap<>();
+    private final UUID mainRoomId;
+    private final UUID mainChatId;
+    private static final Logger logger = Logger.getLogger(GamesRoomManager.class);
+
+
+    GamesRoomManager() {
+        GamesRoom mainRoom = new GamesRoomImpl();
+        mainRoomId = mainRoom.getRoomId();
+        mainChatId = mainRoom.getChatId();
+        rooms.put(mainRoomId, mainRoom);
+    }
+
+    public UUID createRoom() {
+        GamesRoom room = new GamesRoomImpl();
+        rooms.put(room.getRoomId(), room);
+        return room.getRoomId();
+    }
+
+    public UUID getMainRoomId() {
+        return mainRoomId;
+    }
+
+    public UUID getMainChatId() {
+        return mainChatId;
+    }
+
+    public Optional<GamesRoom> getRoom(UUID roomId) {
+        if (rooms.containsKey(roomId)) {
+            return Optional.of(rooms.get(roomId));
+        }
+        logger.error("room not found : " + roomId);
+        return Optional.empty();
+
+    }
+
+    public void removeTable(UUID tableId) {
+        for (GamesRoom room : rooms.values()) {
+            room.removeTable(tableId);
+        }
+    }
+
+}
